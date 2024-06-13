0e50ab2c84559401e703e1974592c65043951862
Dan Chui <dchui108@gmail.com>
2024-06-13 09:21:37 +0900

    Fix Rating average and Edit movie titles

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 var API_KEY = config.MY_API;
 
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" +
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=' +
   API_KEY;
 
-const IMG_PATH = "https://image.tmdb.org/t/p/w500";
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
 
 const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key=" + API_KEY + '&query="';
+  'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query="';
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-const redseatsEl = document.getElementById("redseats");
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const redseatsEl = document.getElementById('redseats');
 
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const current = document.getElementById("current");
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const current = document.getElementById('current');
 
 var currentPage = 1;
 var nextPage = 2;
 var prevPage = 3;
-var lastUrl = "";
+var lastUrl = '';
 var totalPages = 100;
 
 // Get initial movies
@@ -40,17 +40,17 @@ async function getMovies(url) {
         current.innerText = currentPage;
 
         if (currentPage <= 1) {
-          prev.classList.add("disabled");
-          next.classList.remove("disabled");
+          prev.classList.add('disabled');
+          next.classList.remove('disabled');
         } else if (currentPage >= totalPages) {
-          prev.classList.remove("disabled");
-          next.classList.add("disabled");
+          prev.classList.remove('disabled');
+          next.classList.add('disabled');
         } else {
-          prev.classList.remove("disabled");
-          next.classList.remove("disabled");
+          prev.classList.remove('disabled');
+          next.classList.remove('disabled');
         }
 
-        redseatsEl.scrollIntoView({ behavior: "smooth" });
+        redseatsEl.scrollIntoView({ behavior: 'smooth' });
       } else {
         main.innerHTML = `<h1 class="no-results">No Results Found</h1>`;
       }
@@ -60,19 +60,20 @@ async function getMovies(url) {
 getMovies(API_URL);
 
 function showMovies(movies) {
-  main.innerHTML = "";
+  main.innerHTML = '';
 
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie;
+    const rounded = Math.round(vote_average * 10) / 10;
 
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
 
     movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
           <h3>${title}</h3>
-          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+          <span class="${getClassByRate(rounded)}">${rounded}</span>
             </div>
             <div class="overview">
           <h3>Overview</h3>
@@ -86,24 +87,24 @@ function showMovies(movies) {
 // Movie rating
 function getClassByRate(vote) {
   if (vote >= 7.5) {
-    return "green";
+    return 'green';
   } else if (vote >= 5) {
-    return "orange";
+    return 'orange';
   } else {
-    return "red";
+    return 'red';
   }
 }
 
 // Submit function
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const searchTerm = search.value;
 
-  if (searchTerm && searchTerm !== "") {
+  if (searchTerm && searchTerm !== '') {
     getMovies(SEARCH_API + searchTerm);
 
-    search.value = "";
+    search.value = '';
     closeNav();
   } else {
     window.location.reload();
@@ -112,13 +113,13 @@ form.addEventListener("submit", (e) => {
 });
 
 // Pagination
-prev.addEventListener("click", () => {
+prev.addEventListener('click', () => {
   if (prevPage > 0) {
     pageCall(prevPage);
   }
 });
 
-next.addEventListener("click", () => {
+next.addEventListener('click', () => {
   if (nextPage <= totalPages) {
     pageCall(nextPage);
   }
@@ -126,28 +127,28 @@ next.addEventListener("click", () => {
 
 // Query function
 function pageCall(page) {
-  let urlSplit = lastUrl.split("?");
-  let queryPar = urlSplit[1].split("&");
-  let address = queryPar[queryPar.length - 1].split("=");
-  if (address[0] != "page") {
-    let url = lastUrl + "&page=" + page;
+  let urlSplit = lastUrl.split('?');
+  let queryPar = urlSplit[1].split('&');
+  let address = queryPar[queryPar.length - 1].split('=');
+  if (address[0] != 'page') {
+    let url = lastUrl + '&page=' + page;
     getMovies(url);
   } else {
     address[1] = page.toString();
-    let firstPart = address.join("=");
+    let firstPart = address.join('=');
     queryPar[queryPar.length - 1] = firstPart;
-    let secondPart = queryPar.join("&");
-    let url = urlSplit[0] + "?" + secondPart;
+    let secondPart = queryPar.join('&');
+    let url = urlSplit[0] + '?' + secondPart;
     getMovies(url);
   }
 }
 
 // Open the sidenav
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById('mySidenav').style.width = '100%';
 }
 
 // Close/hide the sidenav
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById('mySidenav').style.width = '0';
 }
